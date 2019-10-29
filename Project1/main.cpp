81dd6176f8a52aacb083ececef5cfacfa78f04d3
David Haverland <nerdatron.end@gmail.com>
2019-10-29 18:07:19 +1300

    Broke everything and made ith white theme

---
+++ Project1/main.cpp
@@ -0,0 +1,41 @@
+//
+// Bachelor of Software Engineering
+// Media Design School
+// Auckland
+// New Zealand
+//
+// (c) 2019 Media Design School
+//
+// File Name   : main.cpp
+// Description : Main file
+// Author      : Andrew Barnes and David Haverland
+// Mail        : andrew.bar8456@mediadesign.school.nz and david.hav8466@mediadesign.school.nz
+//
+
+
+
+// All the libraries
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <Windows.h>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+{
+	// Set the consloe to white mode
+	system("color f0");
+
+
+
+
+	
+	int iTemp = 0;
+	cin >> iTemp;
+	return 0;
+}
