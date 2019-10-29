47f05c4dd57ae5826561347c00dd6cc3ee32787c
David Haverland <nerdatron.end@gmail.com>
2019-10-29 23:30:27 +1300

    Started on ui

---
+++ Project1/display.h
@@ -0,0 +1,79 @@
+//
+// Bachelor of Software Engineering
+// Media Design School
+// Auckland
+// New Zealand
+//
+// (c) 2019 Media Design School
+//
+// File Name   : main.cpp
+// Description : Main file
+// Author      : Andrew Barnes and David Haverland
+// Mail        : andrew.bar8456@mediadesign.school.nz and david.hav8466@mediadesign.school.nz
+//
+
+
+#pragma once
+
+
+#include <iostream>
+
+using namespace std;
+
+// Display Things
+void DisplayMenu()
+{
+	system("cls");
+
+	cout << endl;
+	cout << endl;
+	cout << "	Algorithms and Data Structures: LightMode" << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << "	Please input a number below" << endl;
+	cout << endl;
+	cout << "	1. A* algorithm" << endl;
+	cout << endl;
+	cout << "	2. Breadth-First Search" << endl;
+	cout << endl;
+	cout << "	3. Depth-First Search" << endl;
+	cout << endl;
+	cout << "	4. Quit" << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+
+}
+
+void DisplayMenuError()
+{
+	system("cls");
+
+	cout << endl;
+	cout << endl;
+	cout << "	Algorithms and Data Structures: LightMode" << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << "	Please input a number below" << endl;
+	cout << endl;
+	cout << "	1. A* algorithm" << endl;
+	cout << endl;
+	cout << "	2. Breadth-First Search" << endl;
+	cout << endl;
+	cout << "	3. Depth-First Search" << endl;
+	cout << endl;
+	cout << "	4. Quit" << endl;
+	cout << endl;
+	cout << endl;
+	cout << "  Error - Pleas enter a valid input" << endl;
+	cout << endl;
+
+}
+
+
+
+
+
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -22,19 +22,106 @@
 #include <stdlib.h>
 #include <string>
 
+#include "display.h"
+
 using namespace std;
 
+void AStarAlgorithm()
+{
+
+}
+
+void BreadthAlgorithm()
+{
+
+}
+
+void DepthAlgorithm()
+{
+
+}
+
+
+
+void InputMenu() 
+{
+
+	bool bErrorThrown = false;
+
+
+Start:
+	if (bErrorThrown == false)
+	{
+		DisplayMenu();
+	}
+	else
+	{
+		DisplayMenuError();
+	}
+	
+	
+	// Inputs
+	int iInput;
+	cin >> iInput;
+
+	// A* algorithm
+	if (iInput == 1) 
+	{
+		
+	}
+
+	// Breadth-First Search
+	else if (iInput == 2)
+	{
+
+		
+	}
+
+	// Depth-First Search
+	else if (iInput == 3) 
+	{
+
+		
+	}
+	
+
+	//Exit function
+	else if (iInput == 4) 
+	{
+		exit(0);
+	}
+
+	// recalls this function 
+	else 
+	{
+		bErrorThrown = true;
+
+		// Clears the input
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		goto Start;
+	}
+}
 
 
 int main()
 {
-	// Set the consloe to white mode
+	
 	system("color f0");
 
 
 
+	while (true)
+	{
 
+		// Main Menu
+		InputMenu();
 	
+	}
+
+
+
+	Sleep(5000);
 	int iTemp = 0;
 	cin >> iTemp;
 	return 0;
